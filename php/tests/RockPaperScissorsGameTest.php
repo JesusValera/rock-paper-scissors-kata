b1802171b23547d330b90da6f78f125614b53587
JesusValera <jesus1902@outlook.com>
2022-11-25 18:21:40 +0100

    rocks_beats_scissors

---
+++ php/tests/RockPaperScissorsGameTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KataTests;
+
+use Kata\RockPaperScissorsGame;
+use PHPUnit\Framework\TestCase;
+
+class RockPaperScissorsGameTest extends TestCase
+{
+    /** @test */
+    public function rocks_beats_scissors(): void
+    {
+        $game = new RockPaperScissorsGame();
+
+        $result = $game->play('rock', 'scissor');
+
+        self::assertSame('Rock wins', $result);
+    }
+}
